c0eacd5e32ddad751df72d4047b4cfd6f87ade33
Seunghan Han <seunghan.han@valtech.de>
2018-10-04 16:51:23 +0200

    first draft

--- src/main.cpp
+++ src/main.cpp
@@ -85,22 +85,67 @@ int main() {
         string event = j[0].get<string>();
         if (event == "telemetry") {
           // j[1] is the data JSON object
+
+	  // Get waypoints
           vector<double> ptsx = j[1]["ptsx"];
           vector<double> ptsy = j[1]["ptsy"];
+	  
+	  // Get state = [ x, y, psi, v]
           double px = j[1]["x"];
           double py = j[1]["y"];
           double psi = j[1]["psi"];
           double v = j[1]["speed"];
+	  
+	  // Get cte and epsi
+          // Transforming waypoint coordinates to coordinates in the vehicle's coordinate system
+          Eigen::VectorXd ptsx_car(ptsx.size());
+          Eigen::VectorXd ptsy_car(ptsy.size());
+          for(int i = 0;  i < ptsx.size();  i++ ) {
 
+            // relative distance between map waypoint and car position
+            double carcord_x = ptsx[i] - px;
+	    double carcord_y = ptsy[i] - py;
+
+            // Performing the homogeneous transformation from the car's point of view (=base is (0,0))
+            ptsx_car[i] = 0.0 + cos(-psi) * carcord_x - sin(-psi) * carcord_y;
+            ptsy_car[i] = 0.0 + sin(-psi) * carcord_x + cos(-psi) * carcord_y;
+          }
+	  
+	  Eigen::VectorXd pts_car_coeffs = polyfit(ptsx_car,ptsy_car,3);
+	  // The cross track error is calculated by evaluating at polynomial at x, f(x)
+	  // and subtracting y.
+	  double cte = polyeval(pts_car_coeffs, px) - py;
+	  // Due to the sign starting at 0, the orientation error is -f'(x).
+	  // derivative of coeffs[0] + coeffs[1] * x -> coeffs[1]
+	  double epsi = psi - atan(pts_car_coeffs[1]);
+	  
+	  // Given the spec from Data.md
+	  // Get control inputs = [ delta, a]
+	  double delta = j[1]["steering_angle"];
+	  double a = j[1]["throttle"];
+
+	  Eigen::VectorXd state(6);
+	  state << px, py, psi, v, cte, epsi;
+	  
+	  Eigen::VectorXd control(2);
+	  control << delta, a;
+	  
           /*
           * TODO: Calculate steering angle and throttle using MPC.
           *
           * Both are in between [-1, 1].
           *
           */
-          double steer_value;
-          double throttle_value;
+	  auto vars = mpc.Solve(state, pts_car_coeffs);
+    
+          double steer_value = vars[6];
+          double throttle_value = vars[7];
 
+	  /////	  /////	  /////	  /////	  /////	  /////	  /////	  /////	  /////	  /////	  /////
+	  
+	  /////	  /////	  /////	  /////	  /////	  /////	  /////	  /////	  /////	  /////	  /////
+
+	  
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
           // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
